bfe7765a5da5a88bbaaf44a8f0138601c1124af4
Sudharsan Chandrasekaran <sxspringxc24.javadev.own@gmail.com>
2024-08-24 11:36:48 +0530

    Update DemoController.java

--- main/java/com/userpassword/security/controllers/DemoController.java
+++ main/java/com/userpassword/security/controllers/DemoController.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @Autowired
+    private UserRepp repo;
+    
     @GetMapping("/demo")
     public String demo(HttpServletRequest request) {
         return "spring Security "+request.getSession().getId();
+    }
+    @PostMapping("/adduser")
+    public String adduser(@RequestBody DemoUser user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10); // 2^10
+        user.setPassword(encoder.encode(user.getPassword())); // hashed and saved to db
+        repo.save(user);
+        return user.toString();
     }
 }
